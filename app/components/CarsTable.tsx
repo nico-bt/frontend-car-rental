53ef7d7e18d7ddc53d7d2e29a7c128958da8cae7
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-11-07 12:26:59 -0300

    Get cars and show in table

---
+++ app/components/CarsTable.tsx
@@ -0,0 +1,113 @@
+"use client"
+import * as React from "react"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
+import { CarType } from "../page"
+import EditIcon from "@mui/icons-material/Edit"
+import DeleteIcon from "@mui/icons-material/Delete"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+const rowHead = [
+  "marca",
+  "modelo",
+  "año",
+  "km",
+  "color",
+  "AC",
+  "pasajeros",
+  "cambios",
+  "edit",
+  "delete",
+]
+
+export default function CarsTable({ cars }: { cars: CarType[] }) {
+  const [isLoadingItemWithId, setIsLoadingItemWithId] = useState<number | null>(null)
+  const router = useRouter()
+
+  const handleEditClick = (id: number) => {
+    router.push("/edit-car/" + id)
+  }
+
+  const handleDeleteClick = async (id: number) => {
+    setIsLoadingItemWithId(id)
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/car/` + id, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      if (response.ok) {
+        setIsLoadingItemWithId(null)
+        router.refresh()
+      } else {
+        throw new Error()
+      }
+    } catch (error) {
+      console.log(error)
+      setIsLoadingItemWithId(null)
+    }
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="caption table">
+        <TableHead>
+          <TableRow sx={{ backgroundColor: "lightgoldenrodyellow", fontVariantCaps: "small-caps" }}>
+            {rowHead.map((item) => (
+              <TableCell key={item}>{item}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {cars.length === 0 ? (
+            <TableRow>
+              <TableCell align="center" colSpan={10} sx={{ fontSize: "20px" }}>
+                No cars added
+              </TableCell>
+            </TableRow>
+          ) : (
+            cars.map((car) => (
+              <TableRow key={car.id}>
+                <TableCell>{car.marca}</TableCell>
+                <TableCell>{car.modelo}</TableCell>
+                <TableCell>{car.year}</TableCell>
+                <TableCell>{car.km}</TableCell>
+                <TableCell>{car.color}</TableCell>
+                <TableCell align="center">{car.ac ? "si" : "no"}</TableCell>
+                <TableCell align="center">{car.pasajeros}</TableCell>
+                <TableCell>{car.cambios}</TableCell>
+                <TableCell>
+                  <EditIcon
+                    className="actionIcon"
+                    onClick={() => {
+                      handleEditClick(car.id)
+                    }}
+                  />
+                </TableCell>
+                <TableCell>
+                  {isLoadingItemWithId === car.id ? (
+                    "loading..."
+                  ) : (
+                    <DeleteIcon
+                      className="actionIcon"
+                      onClick={() => {
+                        handleDeleteClick(car.id)
+                      }}
+                    />
+                  )}
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,27 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import CarsTable from "./components/CarsTable"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export interface CarType {
+  id: number
+  marca: string
+  modelo: string
+  year: number
+  km: number
+  color: string
+  ac: boolean
+  pasajeros: number
+  cambios: "MANUAL" | "AUTOMATICO"
+  created_at: string
+  updated_at: string
+}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const fetchCars = async (url: string): Promise<CarType[]> => {
+  const res = await fetch(url, { next: { revalidate: 0 } })
+  const cars = await res.json()
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  return cars
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+export default async function Home() {
+  const cars = await fetchCars(`${process.env.NEXT_PUBLIC_BASE_URL_API}/car/`)
+  return <CarsTable cars={cars} />
 }
